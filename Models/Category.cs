b1f7844447b294e02dc75ebd931f0c20dcf6ba91
Jeav29 <60790581+Jeav29@users.noreply.github.com>
2025-02-27 17:54:38 -0600

    Ajustes últimos

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_Imagen.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Category Name")]
+        public string CategoryName { get; set; }
+
+        public ICollection<SubCategory> SubCategories { get; set; }
+    }
+
+}
---
+++ Models/SubCategory.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace CRUD_Imagen.Models
+{
+    public class SubCategory
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "SubCategory Name")]
+        public string SubCategoryName { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
